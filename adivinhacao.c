c105b20e1dab06984dd15e773922b4369449ad88
allanmori21 <allanmori@hotmail.com>
2023-01-09 15:06:24 -0300

    desenvolvendo o código

--- adivinhacao.c
+++ adivinhacao.c
@@ -4,4 +4,20 @@ int main (){
     printf ("******************************************\n");
     printf ("* Bem vindo ao nosso jogo de adivinhação *\n");
     printf ("******************************************\n");
+
+    int numerosecreto = 42;
+    int chute;
+
+    printf ("Qual é o seu chute: ");
+    scanf ("%d", &chute);
+    printf ("Seu chute foi: %d", chute);
+
+    if(chute == numerosecreto) {
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue de novo, você é um bom jogador!\n");
+    }
+    else {
+        printf("Você errou!\n");
+        printf("Mas não desanime, tente de novo!\n");
+}
 }
