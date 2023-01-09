486c689eeaa06de141ae632fe4f4f2aa52617da5
allanmori21 <allanmori@hotmail.com>
2023-01-09 17:09:18 -0300

    Acrescentando pontuações no código

--- adivinhacao.c
+++ adivinhacao.c
@@ -5,23 +5,23 @@ int main (){
 
     int numerosecreto = 42;
     int chute;
-    int i;
     int tentativas = 1;
+    int pontos = 1000;
 
     while (1){
         printf ("Tentativa %d\n", tentativas);
         printf ("Qual eh o seu chute: ");
         scanf ("%d", &chute);
 
-        int acertou = (chute == numerosecreto);
-        int maior = chute > numerosecreto;
-
         if (chute < 0){
             printf("Voce nao pode chutar numeros negativos!\n");
             continue;
         }
 
-        else if(acertou) {
+        int acertou = (chute == numerosecreto);
+        int maior = chute > numerosecreto;
+
+        if(acertou) {
             printf("Parabens! Voce acertou!\n");
             break;
         }
@@ -34,7 +34,11 @@ int main (){
             }
 
         tentativas ++;
+
+        int pontosperdidos = (chute - numerosecreto) / 2;
+        pontos = pontos - pontosperdidos; 
     }    
     printf ("Fim de jogo!\n");
     printf ("Voce acertou em %d tentativas!\n", tentativas);
+    printf ("Total de pontos: %d\n", pontos);
 }
