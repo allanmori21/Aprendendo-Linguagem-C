9a18b2389a9309989a0a0ae4b2ffebc36a427884
allanmori21 <allanmori@hotmail.com>
2023-01-09 16:44:11 -0300

    Melhorando o código usando o while

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
-#define NUMERO_DE_TENTATIVAS 5
-
 int main (){
     printf ("Bem vindo ao nosso jogo de adivinhacao\n");
 
     int numerosecreto = 42;
     int chute;
     int i;
+    int tentativas = 1;
 
-    for (i = 1; i <= NUMERO_DE_TENTATIVAS; i++){
-        printf ("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+    while (1){
+        printf ("Tentativa %d\n", tentativas);
         printf ("Qual eh o seu chute: ");
         scanf ("%d", &chute);
 
         int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
-        int menos = chute < numerosecreto;
 
-        if(acertou) {
+        if (chute < 0){
+            printf("Voce nao pode chutar numeros negativos!\n");
+            continue;
+        }
+
+        else if(acertou) {
             printf("Parabens! Voce acertou!\n");
             break;
         }
@@ -26,10 +29,12 @@ int main (){
         else if(maior) {
             printf("Seu chute foi maior que o numero secreto. Tente novamente!\n");
             }
-        else if (menor) {
+        else {
                 printf("Seu chute foi menor que o numero secreto. Tente novamente!\n");
             }
-        }
+
+        tentativas ++;
     }    
     printf ("Fim de jogo!\n");
+    printf ("Voce acertou em %d tentativas!\n", tentativas);
 }
