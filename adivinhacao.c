fea28f77de414c9e8b7077cc002e6fe6e3d4d7e9
allanmori21 <allanmori@hotmail.com>
2023-01-09 17:53:14 -0300

    Adicionando as funções rand e srand no código

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main (){
     printf ("Bem vindo ao nosso jogo de adivinhacao\n");
 
-    int numerosecreto = 42;
+    int segundos = time(0);
+    srand(segundos);
+    int numerogrande = rand();
+    int numerosecreto = numerogrande % 100;
     int chute;
     int tentativas = 1;
     double pontos = 1000;
